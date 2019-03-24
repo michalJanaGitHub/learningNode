333951998722ad59ff90327b534137f6d59c40d1
TTOPraha <majkl.jana@gmail.com>
2019-03-24 21:28:57 +0100

    Export Modules

---
+++ server.js
@@ -0,0 +1,16 @@
+var questionHandler = require('./questionHandler');
+
+var questions = [
+    "Comment t'appelles-tu ? ",
+    "Que fais-tu dans la vie ? ",
+    "Quel est ton langage de programmation préféré ? "
+];
+
+var attributes = ['name', 'hobby', 'lg'];
+
+questionHandler
+  .setQuestions(questions)
+  .setAttributes(attributes)
+  .ask();
+
+
