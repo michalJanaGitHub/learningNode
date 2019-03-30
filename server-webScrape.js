824b7fbe277cfbbbb4c5e3025b495d3fa4109ba0
TTOPraha <majkl.jana@gmail.com>
2019-03-30 12:02:02 +0100

    create webscrape file

--- server-webScrape.js
+++ server-webScrape.js
@@ -13,24 +13,24 @@ let options = {
 let req = https.request(options, (res) => {
   let responseBody = '';
 
-  console.log('Start');
+  console.log(`Start`);
   // console.log(res.statusCode);
   // console.log(res.headers);
 
   res.setEncoding('UTF-8');
   
   res.on('data', (chunk) => {
-    // console.log(chunk.length);
+    console.log(chunk.length);
     responseBody += chunk;
   });
 
   res.on('end', () => {
-    // fs.writeFileSync('nodejs.html', responseBody);
-    // console.log('File created');
+    fs.writeFileSync('webScrape.html', responseBody);
+    console.log('File created');
     
     const dom = new JSDOM( responseBody);
-    console.log(dom.window.document.getElementsByTagName("p")[3].innerHTML);  
-    // console.log(responseBody);
+    console.log(dom.window.document.getElementsByTagName("p")[3].textContent);  
+    console.log(dom.window.document.querySelectorAll("p")[3].textContent);
   });
 });
 req.end();
