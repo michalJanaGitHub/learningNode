1688528fcfb9e3f2a33b69957319a802b2b5b001
TTOPraha <majkl.jana@gmail.com>
2019-03-31 15:40:57 +0200

    server heml - serve static files

--- s-CreateServer.js
+++ s-CreateServer.js
@@ -2,13 +2,22 @@ let fs = require('fs');
 let http = require('http');
 
 let server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-Type': 'text/plain' });
+  res.writeHead(200, { 'content-Type': 'text/html' });
   
-  res.end('Je suis un serveur HTTP');
-  
+  let responseBody = `
+    <html>
+      <head></head>
+      <h1>Je suis un serveur HTTP</h1>
+      <p>${req.url}</p>
+      <p>${req.method}</p>
+      </body>
+    </html>  
+  `;
+
+  res.end(responseBody);
 }); 
 
 server.listen(3000);
 
-console.log('server on http://localhost:3000')
+console.log('server on http://localhost:3000');
 
