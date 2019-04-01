0bad5087309bb8fcd96095d319846e465c779a25
TTOPraha <majkl.jana@gmail.com>
2019-04-01 21:32:29 +0200

    finish express.js

--- s-express-Static-Get-Post.js
+++ s-express-Static-Get-Post.js
@@ -6,20 +6,19 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));  // middleware
 app.use(express.static('./public'));  // middleware
 
-app.get('/hello', (req, res) => {
+app.get('/submitForm/', (req, res) => {
   console.log(req.query);
-  res.send('Hello World!');
+  console.log(req.method);
+  res.send('FormSubmitted');
 });
 
-app.post('/', (req, res) => {
-  
+app.post('/', (req, res) => {  
   let data = req.body;
   res.send(`
     <h1>Formulaire envoyé avec data :</h1>
     <p> ${data.username} / ${data.password} / ${data.date}    
     `
-    );
-  
+    );  
 });
 
 app.listen(port, () => {
